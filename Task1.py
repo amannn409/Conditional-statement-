6c2fc4e34a7763005eda5703eddc341d757524b6
amannn409 <cuteaman03409@gmail.com>
2025-07-01 16:47:33 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,16 @@
+#Check a number is Even or Odd.
+
+# Taking user input
+number =int(input('Enter a number: '))
+
+# Condition Checking
+
+# if the number is divisible by 2, then even
+# else number is odd
+
+if (number%2==0):
+    print(number, "is Even.")
+else:
+    print(number, "is Odd.")
+
+    print("Thank You")
---
+++ Task2.py
@@ -0,0 +1,13 @@
+# Sum of integers from 1 to 50 Using a Loop
+
+# initialize a variable to store the sum
+
+sum = 0 
+
+# loop through numbers 1 to 50
+# we use 51 to include 50
+
+for i in range (1,51):
+       
+       sum += i
+print(sum)
